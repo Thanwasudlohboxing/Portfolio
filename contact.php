b2a5d85943da5f76cd4496cf09f0e48139e2ff9b
Thanwasudlohboxing <143980452+Thanwasudlohboxing@users.noreply.github.com>
2024-02-01 10:24:31 +0700

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php include('assets/navbar/navbar.php') ?>
+    <link rel="stylesheet" href="assets/navbar/navbar.css">
+    <link rel="stylesheet" href="contact.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+    <div class="main">
+        <div class="facebook">
+            <a href="https://www.facebook.com/profile.php?id=61553483496185"><i class="fa-brands fa-facebook-f fa-2xl" style="font-size: 200px;"></i></a>
+            <a href="https://www.instagram.com/decemberlnwza007/"><i class="fa-brands fa-instagram fa-2xl" style="font-size: 200px;"></i></a>
+        </div>
+        <div class="instagram">
+            
+        </div>
+    </div>
+    <script src="assets/navbar/navbar.js"></script>
+</body>
+
+</html>
